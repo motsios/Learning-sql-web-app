83c75631a35ff1a471ba615cc1ceade717cffd3b
Dimitris  Motsios <dmotsios@terracom.gr>
2021-02-11 20:37:26 +0200

    correct date now from utc and translate to greek

--- src/app/myscore/myscore.component.ts
+++ src/app/myscore/myscore.component.ts
@@ -30,9 +30,148 @@ export class MyscoreComponent implements OnInit {
       })
       .subscribe((data) => {
         this.spinner.hide();
-
         console.log(data.result[0].score_tables);
         if (data.result) {
+          for (var i = 0; i < data.result[0].score_tables.length; i++) {
+            data.result[0].score_tables[i].createdAt = new Date(
+              data.result[0].score_tables[i].createdAt
+            ).toString();
+
+            if (data.result[0].score_tables[i].createdAt.includes('Mon'))
+              data.result[0].score_tables[
+                i
+              ].createdAt = data.result[0].score_tables[i].createdAt.replace(
+                'Mon',
+                'Δευτέρα'
+              );
+            else if (data.result[0].score_tables[i].createdAt.includes('Tue'))
+              data.result[0].score_tables[
+                i
+              ].createdAt = data.result[0].score_tables[i].createdAt.replace(
+                'Tue',
+                'Τρίτη'
+              );
+            else if (data.result[0].score_tables[i].createdAt.includes('Wed'))
+              data.result[0].score_tables[
+                i
+              ].createdAt = data.result[0].score_tables[i].createdAt.replace(
+                'Wed',
+                'Τετάρτη'
+              );
+            else if (data.result[0].score_tables[i].createdAt.includes('Thu'))
+              data.result[0].score_tables[
+                i
+              ].createdAt = data.result[0].score_tables[i].createdAt.replace(
+                'Thu',
+                'Πέμπτη'
+              );
+            else if (data.result[0].score_tables[i].createdAt.includes('Fri'))
+              data.result[0].score_tables[
+                i
+              ].createdAt = data.result[0].score_tables[i].createdAt.replace(
+                'Fri',
+                'Παρασκευή'
+              );
+            else if (data.result[0].score_tables[i].createdAt.includes('Sat'))
+              data.result[0].score_tables[
+                i
+              ].createdAt = data.result[0].score_tables[i].createdAt.replace(
+                'Sat',
+                'Σάββατο'
+              );
+            else if (data.result[0].score_tables[i].createdAt.includes('Sun'))
+              data.result[0].score_tables[
+                i
+              ].createdAt = data.result[0].score_tables[i].createdAt.replace(
+                'Sun',
+                'Κυριακή'
+              );
+
+            if (data.result[0].score_tables[i].createdAt.includes('Jan'))
+              data.result[0].score_tables[
+                i
+              ].createdAt = data.result[0].score_tables[i].createdAt.replace(
+                'Jan',
+                'Ιανουάριος'
+              );
+            else if (data.result[0].score_tables[i].createdAt.includes('Feb'))
+              data.result[0].score_tables[
+                i
+              ].createdAt = data.result[0].score_tables[i].createdAt.replace(
+                'Feb',
+                'Φεβρουάριος'
+              );
+            else if (data.result[0].score_tables[i].createdAt.includes('Mar'))
+              data.result[0].score_tables[
+                i
+              ].createdAt = data.result[0].score_tables[i].createdAt.replace(
+                'Mar',
+                'Μάρτιος'
+              );
+            else if (data.result[0].score_tables[i].createdAt.includes('Apr'))
+              data.result[0].score_tables[
+                i
+              ].createdAt = data.result[0].score_tables[i].createdAt.replace(
+                'Apr',
+                'Απρίλιος'
+              );
+            else if (data.result[0].score_tables[i].createdAt.includes('May'))
+              data.result[0].score_tables[
+                i
+              ].createdAt = data.result[0].score_tables[i].createdAt.replace(
+                'May',
+                'Μάιος'
+              );
+            else if (data.result[0].score_tables[i].createdAt.includes('Jun'))
+              data.result[0].score_tables[
+                i
+              ].createdAt = data.result[0].score_tables[i].createdAt.replace(
+                'Jun',
+                'Ιούνιος'
+              );
+            else if (data.result[0].score_tables[i].createdAt.includes('Jul'))
+              data.result[0].score_tables[
+                i
+              ].createdAt = data.result[0].score_tables[i].createdAt.replace(
+                'Jul',
+                'Ιούλιος'
+              );
+            else if (data.result[0].score_tables[i].createdAt.includes('Aug'))
+              data.result[0].score_tables[
+                i
+              ].createdAt = data.result[0].score_tables[i].createdAt.replace(
+                'Aug',
+                'Αύγουστος'
+              );
+            else if (data.result[0].score_tables[i].createdAt.includes('Sep'))
+              data.result[0].score_tables[
+                i
+              ].createdAt = data.result[0].score_tables[i].createdAt.replace(
+                'Sep',
+                'Σεπτέμβριος'
+              );
+            else if (data.result[0].score_tables[i].createdAt.includes('Oct'))
+              data.result[0].score_tables[
+                i
+              ].createdAt = data.result[0].score_tables[i].createdAt.replace(
+                'Oct',
+                'Οκτώβριος'
+              );
+            else if (data.result[0].score_tables[i].createdAt.includes('Nov'))
+              data.result[0].score_tables[
+                i
+              ].createdAt = data.result[0].score_tables[i].createdAt.replace(
+                'Nov',
+                'Νοέμβριος'
+              );
+            else if (data.result[0].score_tables[i].createdAt.includes('Dec'))
+              data.result[0].score_tables[
+                i
+              ].createdAt = data.result[0].score_tables[i].createdAt.replace(
+                'Dec',
+                'Δεκέμβριος'
+              );
+          }
           this.scoresArray = data.result[0].score_tables;
           if (data.result[0].score_tables.length == 0) {
             Swal.fire('Oops...', 'Δεν έχετε καταχωρημένα Σκορ!', 'error');
--- src/app/ranking-score/ranking-score.component.ts
+++ src/app/ranking-score/ranking-score.component.ts
@@ -38,6 +38,89 @@ export class RankingScoreComponent implements OnInit {
       .get<any>(this.url.baseUrl + 'bestscores/25 Questions', { headers })
       .subscribe((data) => {
         if (data.result) {
+          for (var i = 0; i < data.result.length; i++) {
+            data.result[i].score_tables[0].createdAt = new Date(
+              data.result[i].score_tables[0].createdAt
+            ).toString();
+
+            if (data.result[i].score_tables[0].createdAt.includes('Mon'))
+              data.result[i].score_tables[0].createdAt = data.result[
+                i
+              ].score_tables[0].createdAt.replace('Mon', 'Δευτέρα');
+            else if (data.result[i].score_tables[0].createdAt.includes('Tue'))
+              data.result[i].score_tables[0].createdAt = data.result[
+                i
+              ].score_tables[0].createdAt.replace('Tue', 'Τρίτη');
+            else if (data.result[i].score_tables[0].createdAt.includes('Wed'))
+              data.result[i].score_tables[0].createdAt = data.result[
+                i
+              ].score_tables[0].createdAt.replace('Wed', 'Τετάρτη');
+            else if (data.result[i].score_tables[0].createdAt.includes('Thu'))
+              data.result[i].score_tables[0].createdAt = data.result[
+                i
+              ].score_tables[0].createdAt.replace('Thu', 'Πέμπτη');
+            else if (data.result[i].score_tables[0].createdAt.includes('Fri'))
+              data.result[i].score_tables[0].createdAt = data.result[
+                i
+              ].score_tables[0].createdAt.replace('Fri', 'Παρασκευή');
+            else if (data.result[i].score_tables[0].createdAt.includes('Sat'))
+              data.result[i].score_tables[0].createdAt = data.result[
+                i
+              ].score_tables[0].createdAt.replace('Sat', 'Σαββατο');
+            else if (data.result[i].score_tables[0].createdAt.includes('Sun'))
+              data.result[i].score_tables[0].createdAt = data.result[
+                i
+              ].score_tables[0].createdAt.replace('Sun', 'Κυριακή');
+
+            if (data.result[i].score_tables[0].createdAt.includes('Jan'))
+              data.result[i].score_tables[0].createdAt = data.result[
+                i
+              ].score_tables[0].createdAt.replace('Jan', 'Ιανουάριος');
+            else if (data.result[i].score_tables[0].createdAt.includes('Feb'))
+              data.result[i].score_tables[0].createdAtt = data.result[
+                i
+              ].score_tables[0].createdAt.replace('Feb', 'Φεβρουάριος');
+            else if (data.result[i].score_tables[0].createdAt.includes('Mar'))
+              data.result[i].score_tables[0].createdAt = data.result[
+                i
+              ].score_tables[0].createdAt.replace('Mar', 'Μάρτιος');
+            else if (data.result[i].score_tables[0].createdAt.includes('Apr'))
+              data.result[i].score_tables[0].createdAt = data.result[
+                i
+              ].score_tables[0].createdAt.replace('Apr', 'Απρίλιος');
+            else if (data.result[i].score_tables[0].createdAt.includes('May'))
+              data.result[i].score_tables[0].createdAt = data.result[
+                i
+              ].score_tables[0].createdAt.replace('May', 'Μάιος');
+            else if (data.result[i].score_tables[0].createdAt.includes('Jun'))
+              data.result[i].score_tables[0].createdAt = data.result[
+                i
+              ].score_tables[0].createdAt.replace('Jun', 'Ιούνιος');
+            else if (data.result[i].score_tables[0].createdAt.includes('Jul'))
+              data.result[i].score_tables[0].createdAt = data.result[
+                i
+              ].score_tables[0].createdAt.replace('Jul', 'Ιούλιος');
+            else if (data.result[i].score_tables[0].createdAt.includes('Aug'))
+              data.result[i].score_tables[0].createdAt = data.result[
+                i
+              ].score_tables[0].createdAt.replace('Aug', 'Αύγουστος');
+            else if (data.result[i].score_tables[0].createdAt.includes('Sep'))
+              data.result[i].score_tables[0].createdAt = data.result[
+                i
+              ].score_tables[0].createdAt.replace('Sep', 'Σεπτέμβριος');
+            else if (data.result[i].score_tables[0].createdAt.includes('Oct'))
+              data.result[i].score_tables[0].createdAt = data.result[
+                i
+              ].score_tables[0].createdAt.replace('Oct', 'Οκτώβριος');
+            else if (data.result[i].score_tables[0].createdAt.includes('Nov'))
+              data.result[i].score_tables[0].createdAt = data.result[
+                i
+              ].score_tables[0].createdAt.replace('Nov', 'Νοέμβριος');
+            else if (data.result[i].score_tables[0].createdAt.includes('Dec'))
+              data.result[i].score_tables[0].createdAt = data.result[
+                i
+              ].score_tables[0].createdAt.replace('Dec', 'Δεκέμβριος');
+          }
           this.bestScoreOf25Array = data.result;
           console.log(this.bestScoreOf25Array);
           this.http
@@ -45,6 +128,123 @@ export class RankingScoreComponent implements OnInit {
             .subscribe((data) => {
               this.spinner.hide();
               if (data.result) {
+                for (var i = 0; i < data.result.length; i++) {
+                  data.result[i].score_tables[0].createdAt = new Date(
+                    data.result[i].score_tables[0].createdAt
+                  ).toString();
+
+                  if (data.result[i].score_tables[0].createdAt.includes('Mon'))
+                    data.result[i].score_tables[0].createdAt = data.result[
+                      i
+                    ].score_tables[0].createdAt.replace('Mon', 'Δευτέρα');
+                  else if (
+                    data.result[i].score_tables[0].createdAt.includes('Tue')
+                  )
+                    data.result[i].score_tables[0].createdAt = data.result[
+                      i
+                    ].score_tables[0].createdAt.replace('Tue', 'Τρίτη');
+                  else if (
+                    data.result[i].score_tables[0].createdAt.includes('Wed')
+                  )
+                    data.result[i].score_tables[0].createdAt = data.result[
+                      i
+                    ].score_tables[0].createdAt.replace('Wed', 'Τετάρτη');
+                  else if (
+                    data.result[i].score_tables[0].createdAt.includes('Thu')
+                  )
+                    data.result[i].score_tables[0].createdAt = data.result[
+                      i
+                    ].score_tables[0].createdAt.replace('Thu', 'Πέμπτη');
+                  else if (
+                    data.result[i].score_tables[0].createdAt.includes('Fri')
+                  )
+                    data.result[i].score_tables[0].createdAt = data.result[
+                      i
+                    ].score_tables[0].createdAt.replace('Fri', 'Παρασκευή');
+                  else if (
+                    data.result[i].score_tables[0].createdAt.includes('Sat')
+                  )
+                    data.result[i].score_tables[0].createdAt = data.result[
+                      i
+                    ].score_tables[0].createdAt.replace('Sat', 'Σάββατο');
+                  else if (
+                    data.result[i].score_tables[0].createdAt.includes('Sun')
+                  )
+                    data.result[i].score_tables[0].createdAt = data.result[
+                      i
+                    ].score_tables[0].createdAt.replace('Sun', 'Κυριακή');
+
+                  if (data.result[i].score_tables[0].createdAt.includes('Jan'))
+                    data.result[i].score_tables[0].createdAt = data.result[
+                      i
+                    ].score_tables[0].createdAt.replace('Jan', 'Ιανουάριος');
+                  else if (
+                    data.result[i].score_tables[0].createdAt.includes('Feb')
+                  )
+                    data.result[i].score_tables[0].createdAtt = data.result[
+                      i
+                    ].score_tables[0].createdAt.replace('Feb', 'Φεβρουάριος');
+                  else if (
+                    data.result[i].score_tables[0].createdAt.includes('Mar')
+                  )
+                    data.result[i].score_tables[0].createdAt = data.result[
+                      i
+                    ].score_tables[0].createdAt.replace('Mar', 'Μάρτιος');
+                  else if (
+                    data.result[i].score_tables[0].createdAt.includes('Apr')
+                  )
+                    data.result[i].score_tables[0].createdAt = data.result[
+                      i
+                    ].score_tables[0].createdAt.replace('Apr', 'Απρίλιος');
+                  else if (
+                    data.result[i].score_tables[0].createdAt.includes('May')
+                  )
+                    data.result[i].score_tables[0].createdAt = data.result[
+                      i
+                    ].score_tables[0].createdAt.replace('May', 'Μάιος');
+                  else if (
+                    data.result[i].score_tables[0].createdAt.includes('Jun')
+                  )
+                    data.result[i].score_tables[0].createdAt = data.result[
+                      i
+                    ].score_tables[0].createdAt.replace('Jun', 'Ιούνιος');
+                  else if (
+                    data.result[i].score_tables[0].createdAt.includes('Jul')
+                  )
+                    data.result[i].score_tables[0].createdAt = data.result[
+                      i
+                    ].score_tables[0].createdAt.replace('Jul', 'Ιούλιος');
+                  else if (
+                    data.result[i].score_tables[0].createdAt.includes('Aug')
+                  )
+                    data.result[i].score_tables[0].createdAt = data.result[
+                      i
+                    ].score_tables[0].createdAt.replace('Aug', 'Αύγουστος');
+                  else if (
+                    data.result[i].score_tables[0].createdAt.includes('Sep')
+                  )
+                    data.result[i].score_tables[0].createdAt = data.result[
+                      i
+                    ].score_tables[0].createdAt.replace('Sep', 'Σεπτέμβριος');
+                  else if (
+                    data.result[i].score_tables[0].createdAt.includes('Oct')
+                  )
+                    data.result[i].score_tables[0].createdAt = data.result[
+                      i
+                    ].score_tables[0].createdAt.replace('Oct', 'Οκτώβριος');
+                  else if (
+                    data.result[i].score_tables[0].createdAt.includes('Nov')
+                  )
+                    data.result[i].score_tables[0].createdAt = data.result[
+                      i
+                    ].score_tables[0].createdAt.replace('Nov', 'Νοέμβριος');
+                  else if (
+                    data.result[i].score_tables[0].createdAt.includes('Dec')
+                  )
+                    data.result[i].score_tables[0].createdAt = data.result[
+                      i
+                    ].score_tables[0].createdAt.replace('Dec', 'Δεκέμβριος');
+                }
                 this.bestScoreOf15Array = data.result;
                 console.log(this.bestScoreOf15Array);
               } else {
