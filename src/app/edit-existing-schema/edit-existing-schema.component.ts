a6d23e274eb7b3016531151ba1c58fe0328f03a9
Dimitris  Motsios <dmotsios@terracom.gr>
2021-06-19 21:27:03 +0300

    correct a mistake

--- src/app/edit-existing-schema/edit-existing-schema.component.ts
+++ src/app/edit-existing-schema/edit-existing-schema.component.ts
@@ -1826,9 +1826,9 @@ export class EditExistingSchemaComponent implements OnInit {
       '<br>IS_NULLABLE: ' +
       d.IS_NULLABLE +
       '<br>REFERENCED_TABLE_NAME: ' +
-      reference_column_name +
+      reference_column_table +
       '<br>REFERENCED_COLUMN_NAME: ' +
-      reference_column_table;
+      reference_column_name;
     Swal.fire({
       title: d.COLUMN_NAME + '(' + d.COLUMN_TYPE + ')',
       html: infoHtmlSting,
